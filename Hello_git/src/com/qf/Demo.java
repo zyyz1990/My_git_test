9be412b351a8b25cd27eccd9596fea63d4e16b82
zhangyang <13335390494@163.com>
2020-11-10 22:42:25 +0800

    把本地idea工程添加到git

---
+++ Hello_git/src/com/qf/Demo.java
@@ -0,0 +1,18 @@
+package com.qf;
+
+import org.junit.Test;
+
+/**
+ * @Author:zhangyang
+ * @Date: 22:22
+ * @Version 1.0
+ */
+public class Demo {
+    @Test
+    public void testHello(){
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+    }
+}
