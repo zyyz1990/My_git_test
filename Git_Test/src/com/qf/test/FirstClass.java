e81b15f3b7a8b160aaf18f010d5f2f599eed43bc
zhangyang_glo <zhangyang666@163.com>
2020-12-06 23:41:25 +0800

    第一次修改了firstClass中的内容

---
+++ Git_Test/src/com/qf/test/FirstClass.java
@@ -0,0 +1,19 @@
+package com.qf.test;
+
+/**
+ * @Author:zhangyang
+ * @Date: 23:29
+ * @Version 1.0
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello,this is my Firstgit add");
+    }
+
+    public void test(){
+        int i = 0 ;
+        i+=100;
+        System.out.println("i:"+i);
+    }
+}
